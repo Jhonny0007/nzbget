6309350d38ba99579b5a3b9e8fe71ba55adcf5b7
dnzbk <denis@nzbget.com>
2024-06-25 11:24:27 +0300

    Fix: warnings

--- daemon/connect/HttpClient.cpp
+++ daemon/connect/HttpClient.cpp
@@ -28,7 +28,7 @@ namespace HttpClient
 	namespace asio = boost::asio;
 	using tcp = boost::asio::ip::tcp;
 
-#ifndef DISABLE_TLS && HAVE_OPENSSL
+#if !defined(DISABLE_TLS) && defined(HAVE_OPENSSL)
 	namespace ssl = boost::asio::ssl;
 #endif
 
@@ -36,7 +36,7 @@ namespace HttpClient
 		: m_context{}
 		, m_resolver{ m_context }
 	{
-#ifndef DISABLE_TLS && HAVE_OPENSSL
+#if !defined(DISABLE_TLS) && defined(HAVE_OPENSSL)
 		m_sslContext.set_default_verify_paths();
 #endif
 	}
@@ -63,7 +63,7 @@ namespace HttpClient
 
 	std::string HttpClient::GetProtocol() const
 	{
-#ifndef DISABLE_TLS && HAVE_OPENSSL
+#if !defined(DISABLE_TLS) && defined(HAVE_OPENSSL)
 		return "https";
 #else
 		return "http";
@@ -85,7 +85,7 @@ namespace HttpClient
 	void HttpClient::Connect(Socket& socket, const Endpoints& endpoints, const std::string& host)
 	{
 		asio::connect(socket.lowest_layer(), endpoints);
-#ifndef DISABLE_TLS && HAVE_OPENSSL
+#if !defined(DISABLE_TLS) && defined(HAVE_OPENSSL)
 		DoHandshake(socket, host);
 #endif
 		SaveLocalIP(socket);
@@ -175,7 +175,7 @@ namespace HttpClient
 		m_localIP = socket.lowest_layer().local_endpoint().address().to_string();
 	}
 
-#ifndef DISABLE_TLS && HAVE_OPENSSL
+#if !defined(DISABLE_TLS) && defined(HAVE_OPENSSL)
 	Socket HttpClient::GetSocket()
 	{
 		return ssl::stream<tcp::socket>{ m_context, m_sslContext };
--- daemon/connect/HttpClient.h
+++ daemon/connect/HttpClient.h
@@ -25,13 +25,13 @@
 #include <thread>
 #include <boost/asio.hpp>
 
-#ifndef DISABLE_TLS && HAVE_OPENSSL
+#if !defined(DISABLE_TLS) && defined(HAVE_OPENSSL)
 #include <boost/asio/ssl.hpp>
 #endif
 
 namespace HttpClient
 {
-#ifndef DISABLE_TLS && HAVE_OPENSSL
+#if !defined(DISABLE_TLS) && defined(HAVE_OPENSSL)
 	using Socket = boost::asio::ssl::stream<boost::asio::ip::tcp::socket>;
 #else
 	using Socket = boost::asio::ip::tcp::socket;
@@ -69,7 +69,7 @@ namespace HttpClient
 		boost::asio::io_context m_context;
 		boost::asio::ip::tcp::resolver m_resolver;
 		std::string m_localIP;
-#ifndef DISABLE_TLS && HAVE_OPENSSL
+#if !defined(DISABLE_TLS) && defined(HAVE_OPENSSL)
 		void DoHandshake(Socket& socket, const std::string& host);
 		boost::asio::ssl::context m_sslContext{ boost::asio::ssl::context::tlsv13_client };
 #endif
